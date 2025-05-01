a3d5c56841a608e2ef73648d6072b9191f636a1c
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2025-05-01 21:59:15 +0200

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Termostat
+Projekt w ramach ideii DIY. Powstał on na potrzeby regulacji temperatury na mieszkaniu, we współpracy z piecem Ariston HS PREMIUM 24 EU2, jednak wiele piecy działa na podobnej zasadzie, a więc urządzenie to jest kompatybilne z wieloma modelami. 
+
+Z pieca można wyprowadzić kabel który po zwarciu uruchamia grzanie, mechanizm działania termostatu jest więc prosty. Mikrokontroler na podstawie danych z czujnika temperatury zmienia stan switcha który jest połączony z piecem za pomocą dwużyłowego kabla. Aby móc łatwo regulować temperaturę termostat został wyposażony w enkoder cyfrowy oraz wyświetlacz.
+
+Użyty czujnik zbiera informacje nie tylko o temperaturze, ale także o wilgotności, dlatego ta informacja jest wyświetlana obok temperatury, temperatury zadanej oraz histerezy.
+
+Projekt płytki PCB można znaleźć w pliku Termostat_PCB.psb, użyte przeze mnie komponenty to:
+mikrokontroler arduino nano
+ wyświetlacz to 1.8 TFT SPI 128x160 V1.1
+ czujnik temperatury i wilgoci AHT10
+ switch JQC-3FF-S-Z
+ Enkoder cyfrowy Cap EC11
---
+++ Termostat.ino
@@ -0,0 +1,186 @@
+#include <Adafruit_AHTX0.h>
+#include <Adafruit_GFX.h> 
+#include <Adafruit_ST7735.h> 
+#include <SPI.h>
+#include <Wire.h>
+
+#define TFT_CS 10
+#define TFT_RST 8
+#define TFT_DC 9
+#define ENC_A 2  // S1
+#define ENC_B 4  // S2
+#define ENC_SW 3 // KEY
+#define SWITCH 7
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_AHTX0 aht;
+
+float T_aht, hum_aht, T_min = 19, T_max = 19.5, hysteresis = 0.5;
+bool heating;
+
+int bpc = 0;
+
+void setup(){
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setRotation(1);
+  printCopyright();
+  Serial.begin(9600);
+  aht.begin();
+  heating = false;
+  pinMode(SWITCH, OUTPUT);
+  pinMode(ENC_A, INPUT);
+  pinMode(ENC_B, INPUT);
+  pinMode(ENC_SW, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENC_A), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC_SW), handleEncoderButton, CHANGE);
+  delay(1000);
+  tft.fillScreen(ST7735_BLACK);
+  drawGUI();
+}
+
+void loop() {
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  T_aht = temp.temperature;
+  hum_aht = humidity.relative_humidity;
+
+  if(heating && (T_aht > T_max)) 
+  {
+    heating = false;
+    digitalWrite(SWITCH, LOW);
+    tft.fillCircle(65, 50, 3, ST7735_BLACK);
+  }
+  else if(!heating && (T_aht < T_min)) 
+  {
+    heating = true;
+    digitalWrite(SWITCH, HIGH);
+    tft.fillCircle(65, 50, 3, ST7735_RED);
+  }
+  showData();
+}
+
+void drawGUI(){
+  for(int i = 0; i < 3; i++){
+    tft.drawRoundRect(i, i, 78 - 2*i, 62 - 2*i, 5, ST7735_RED);
+    tft.drawRoundRect(i, 64 + i, 78 - 2*i, 62 - 2*i, 5, ST7735_BLUE);
+    tft.drawRoundRect(80+i, i, 78 - 2*i, 62 - 2*i, 5, ST7735_GREEN);
+    tft.drawRoundRect(80 + i, 64 + i, 78 - 2*i, 62 - 2*i, 5, ST7735_YELLOW);
+  }
+  
+  tft.setTextSize(1);
+
+  tft.setTextColor(ST7735_RED, ST7735_BLACK);
+  tft.setCursor(5, 5);
+  tft.print("TEMPERATURA");
+
+  tft.setTextColor(ST7735_BLUE, ST7735_BLACK);
+  tft.setCursor(10, 69);
+  tft.print("WILGOTNOSC");
+
+  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
+  tft.setCursor(85, 5);
+  tft.print("TEMPERATURA");
+  tft.setCursor(100, 14);
+  tft.print("ZADANA");
+
+  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);
+  tft.setCursor(92, 69);
+  tft.print("HISTEREZA");
+
+  tft.drawCircle(65, 50, 5, ST7735_RED);
+  tft.drawCircle(145, 50, 5, ST7735_GREEN);
+  tft.drawCircle(145, 114, 5, ST7735_YELLOW);
+  
+  drawSelectedCircle();
+  showData();
+}
+
+void showData(){
+  tft.setTextSize(2);
+
+  tft.setTextColor(ST7735_RED, ST7735_BLACK);
+  tft.setCursor(10,25);
+  tft.print(String(T_aht, 1) + "C");
+
+  tft.setTextColor(ST7735_BLUE, ST7735_BLACK);
+  tft.setCursor(10,90);
+  tft.print(String(hum_aht, 1) + "%");
+
+  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
+  tft.setCursor(90,25);
+  if(T_min > 0 && T_min < 10){
+    tft.print(" " + String(T_min, 1) + "C");
+  }else{
+    tft.print(String(T_min, 1) + "C");
+  }
+
+  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);
+  tft.setCursor(95,90);
+  tft.print(String(hysteresis, 1) + "C");
+}
+
+void printCopyright() {
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
+  tft.println("TERMOSTAT 1.2.0");
+  delay(250);
+  tft.println("DOMINIK MOZDZEN");
+  delay(250);
+  tft.println("2025");
+  delay(250);
+}
+
+void drawSelectedCircle(){
+  if(bpc%2==0){
+    tft.fillCircle(145, 50, 3, ST7735_GREEN);
+  }else{
+    tft.fillCircle(145, 50, 3, ST7735_BLACK);
+  }
+  if(bpc%2==1){
+    tft.fillCircle(145, 114, 3, ST7735_YELLOW);
+  }else{
+    tft.fillCircle(145, 114, 3, ST7735_BLACK);
+  }
+}
+
+void updateEncoder() {
+  switch(bpc%2)
+  {
+    case 0:
+      if(digitalRead(ENC_A)){
+        if(digitalRead(ENC_B)){
+          if(T_min < 99.8){
+            T_min += 0.1;
+          }
+        }
+        else{
+          if(T_min > -9.9)
+          T_min -= 0.1;
+        }
+      T_max = T_min + hysteresis;
+      }
+      break;
+    case 1:
+    if(digitalRead(ENC_A)){
+        if(digitalRead(ENC_B)){
+          if(hysteresis <= 9.9){
+            hysteresis += 0.1;
+          }
+        }
+        else if(hysteresis > 0.1){
+          hysteresis -= 0.1;
+        }
+      T_max = T_min + hysteresis;
+      }
+      break;
+  }
+  
+}
+
+void handleEncoderButton() {
+  if (digitalRead(ENC_SW)==0) {
+    bpc++;
+  }
+  drawSelectedCircle();
+}
